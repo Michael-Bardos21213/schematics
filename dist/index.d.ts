cad8d52eb50b740e9ea16b36efc20fe53a2d7dee
Michael Bardos <michael.bardos21213@gmail.com>
2023-08-11 21:17:29 +0530

    Initial commit

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+import { Rule } from '@angular-devkit/schematics';
+export declare function createOpshubSchematic(options: any): Rule;
+//# sourceMappingURL=index.d.ts.map
---
+++ schematics/create-opshub-schematic/index.ts
@@ -0,0 +1,45 @@
+import { Rule, SchematicContext, Tree, chain } from '@angular-devkit/schematics';
+import { execSync } from 'child_process';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import { join } from 'path';
+
+export function createOpshubSchematic(options: any): Rule {
+  return chain([
+    (tree: Tree, context: SchematicContext) => {
+      const projectName = options.name;
+      const workspacePath = `C:/users/Public/${projectName}`;
+
+      // Create React workspace using create-react-app
+      execSync(`npx create-react-app ${workspacePath}`);
+
+      // Navigate into the newly created workspace directory
+      process.chdir(workspacePath);
+
+      // Copy the file to the src folder
+      const srcPath = join('src', projectName);
+      const filePath = options['file-path'];
+      const fileContents = tree.read(filePath);
+      if (fileContents) {
+        tree.create(join(srcPath, 'enquiry-screen.ts'), fileContents);
+      }
+
+      // Update the package.json file to include package versions
+      const packageJsonPath = 'package.json';
+      const packageJsonBuffer = tree.read(packageJsonPath);
+      if (packageJsonBuffer) {
+        const packageJson = JSON.parse(packageJsonBuffer.toString());
+        packageJson.dependencies = {
+          ...packageJson.dependencies,
+          axios: '^0.21.4',
+          '@mui/material': '^5.0.0'
+        };
+        tree.overwrite(packageJsonPath, JSON.stringify(packageJson, null, 2));
+      }
+
+      // Install required packages using NodePackageInstallTask
+      context.addTask(new NodePackageInstallTask());
+
+      return tree;
+    }
+  ]);
+}
